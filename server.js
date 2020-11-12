4286b0017e3bcab7e7da90a1d134ea6c3465d182
kendrinab <kendra.barrera01@gmail.com>
2020-11-11 22:26:10 -0500

    kendrab-birthday-tracker

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
-require ('./server/db/config/index');
+require('./server/db/config/index');
 const express = require('express');
 const path = require('path');
 const app = express();
